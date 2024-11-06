031597293d79c7d6ddab09e232b4abc94d893e4f
Adit0507 <adi05072002@gmail.com>
2024-11-06 18:12:07 +0530

    compiling fn. calls

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -262,6 +262,13 @@ func (c *Compiler) Compile(node ast.Node) error {
 
 		c.emit(code.OpReturnValue)
 
+	case *ast.CallExpression:
+		err := c.Compile(node.Function)
+		if err != nil {
+			return err
+		}
+		c.emit(code.OpCall)
+
 	case *ast.FunctionLiteral:
 		c.enterScope()
 		err := c.Compile(node.Body)
